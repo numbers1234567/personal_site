c8ffaef6c9e0bcfbfe3e291bd27fc8388bc1a4eb
numbers1234567 <33708611+numbers1234567@users.noreply.github.com>
2024-07-20 18:44:33 -0400

    Implement 3D turn view

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,86 @@
+"use client"
 import Image from "next/image";
+import { TurnView } from "./turn_view";
+import { BackSide, Material, Mesh, MeshBasicMaterial, MeshStandardMaterial, Object3D, PlaneGeometry } from "three";
+import { useLoader } from "@react-three/fiber";
+import { FBXLoader, GLTFLoader } from "three/examples/jsm/Addons.js";
+
+function taiko() {
+  const objbase = useLoader(GLTFLoader, "./taiko.glb");
+  const obj = objbase.scene.clone();
+  obj.scale.setScalar(0.1);
+  obj.traverse((child : Object3D) => {
+    if ("material" in child) console.log(child.material);
+  });
+  obj.rotateY(Math.PI/2)
+  obj.rotateX(Math.PI/4)
+  return obj;
+}
+function gatton() {
+  const objbase = useLoader(GLTFLoader, "./gatton.glb");
+  const obj = objbase.scene.clone();
+  obj.scale.setScalar(0.075);
+  obj.traverse((child : Object3D) => {
+    if ("material" in child) console.log(child.material);
+  });
+  obj.rotateY(Math.PI/2)
+  obj.rotateX(Math.PI/2 - Math.PI/16)
+  return obj;
+}
+function box() {
+  const objbase = useLoader(GLTFLoader, "./luggage.glb");
+  const obj = objbase.scene.clone();
+  obj.scale.setScalar(0.075);
+  obj.traverse((child : Object3D) => {
+    if ("material" in child) console.log(child.material);
+  });
+  obj.rotateY(Math.PI/4)
+  return obj;
+}
+function monitor() {
+  const objbase = useLoader(GLTFLoader, "./monitor.glb");
+  const obj = objbase.scene.clone();
+  obj.scale.setScalar(0.05);
+  obj.traverse((child : Object3D) => {
+    if ("material" in child) console.log(child.material);
+  });
+  obj.rotateY(Math.PI)
+  return obj;
+}
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+  return <main>
+    <div className="h-screen">
+      <TurnView 
+        content={[gatton(), taiko(), box(), monitor()]}
+        text={[`
+          <p>I am a Gatton Academy alumnus.</p>
+          <p>Living away from my family for the first time was a great way for me to transition to my current studies at UT Dallas. Being surrounded by other very smart CS students propelled my problem-solving skills.</p>
+            `,
+            `
+          <p>I study computer science at UT Dallas.</p>
+          <p>My main interest has been machine learning, so I've done research and projects based on ML.</p>
+          <p>Recently I've gotten into web development due to a personal ML project which involves web and my internship.</p>
+          <p>I got a wide monitor, and I swear it actually improves the quality of my work. A funny story is that it ended up being free.</p>
+            `,
+            `
+          <p>My family moved around the nation a bit while I was growing up.</p>
+          <p>Some places I've lived are:</p>
+          <list>
+            <li>Michigan</li>
+            <li>Iowa</li>
+            <li>Utah</li>
+            <li>Alabama (a couple cities)</li>
+            <li>Kentucky (a couple cities)</li>
+          </list>
+            `,
+            `
+          <p>One of my favorite games: Taiko no Tatsujin</p>
+          <p>This game simulates a drum. My favorite part is improving every day and seeing how my scores increase over time. I used to play osu!mania for a year and a half until I got bored. I got pretty decent at it, too.
+            `,
+        ]}
+      />
+    </div>
+  </main>
 }
